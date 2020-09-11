e0209bc11a4d1704f98fd462deee14d26b8c2047
Amonsta <amana.ammi@gmail.com>
2020-09-11 15:54:45 -0600

    updated methods in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,12 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        @user.save
-        redirect_to user_path(@user)
+        if @user.save
+            session[:user_id] = user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def show
