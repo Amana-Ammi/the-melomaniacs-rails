48fb78f70b933a2be37810af3d3ea55d3d2a48ff
Amonsta <amana.ammi@gmail.com>
2020-09-11 19:15:28 -0600

    added some login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+    
+    def new
+    end
+ 
+    def create
+        session[:email] = params[:email]
+        redirect_to '/'
+    end
+
+    #log out
+    def destroy
+        session.delete :email
+    end
+
 end
