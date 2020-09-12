cd9157a2a4095119a14b6f3d82a202e1e231d0f4
Amonsta <amana.ammi@gmail.com>
2020-09-12 12:47:42 -0600

    built login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,20 @@ class SessionsController < ApplicationController
     end
  
     def create
-        session[:email] = params[:email]
-        redirect_to '/'
+       @user = User.find_by(email: params[:user][:email])
+       if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+       else
+        flash[:error] = "Oh No! Looks like you entered incorrect information. Please try again."
+        redirect_to login_path   
+    end
     end
 
     #log out
     def destroy
-        session.delete :email
+        session.delete(:user_id)
+        redirect_to '/'
     end
 
 end
