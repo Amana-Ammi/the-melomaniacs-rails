8aeaec31ac397282de73ea18749388612a38e8cb
Amonsta <amana.ammi@gmail.com>
2020-09-13 12:21:08 -0600

    edit and show functionality to review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,19 @@
 class ReviewsController < ApplicationController
+    
+    def index
+        @reviews = Review.all
+    end
+
+    def show
+        @review = Review.find(params[:id])
+    end
+    
     def new
         @review = Review.new
     end
 
     def create
-        @review = Review.new(review_params)
+        @review = Review.create(review_params)
         if @review.save
             redirect_to review_path(@review)
         else
@@ -15,6 +24,18 @@ class ReviewsController < ApplicationController
     def show
         @review = Review.find(params[:id])
     end
+
+    def edit
+        @review = Review.find(params[:id])
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        @review.update(review_params)
+        redirect_to review_path(@review)
+    end
+
+    
     
 
     private
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
+    belongs_to :user
     has_many :users, through: :reactions
+    
+    validates :title, presence: true
+    validates :title, uniqueness: true
+    validates :content, presence: true
 end
