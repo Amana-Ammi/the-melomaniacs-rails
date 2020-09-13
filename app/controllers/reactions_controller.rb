af16a9b7515c871766a1b0fe909a82e5972aa7ab
Amonsta <amana.ammi@gmail.com>
2020-09-13 15:02:49 -0600

    start of reactions integration

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,2 +1,14 @@
 class ReactionsController < ApplicationController
+
+    def new
+        @review = Review.find_by_id(params[:id])
+        @reaction = @review.reactions.build
+    end
+
+    def create
+    end
+
+    def index
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   resources :users
   # resources :musics
-  # resources :reactions
-  resources :reviews
+  resources :reviews do 
+    resources :reactions, only: [:new, :index]  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
