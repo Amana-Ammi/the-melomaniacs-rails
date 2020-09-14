fbaacbb620ec6437e6d4b0a5b1271dceb1fc360e
Amonsta <amana.ammi@gmail.com>
2020-09-14 14:47:04 -0600

    working fb omniauth

--- app/controllers/fbs_controller.rb
+++ app/controllers/fbs_controller.rb
@@ -2,15 +2,18 @@ class FbsController < ApplicationController
     def create
         @fb = Fb.find_or_create_by(uid: auth['uid']) do |u|
           u.name = auth['info']['name']
-          u.email = auth['info']['email']
+
           u.image = auth['info']['image']
         end
   #    binding.pry
         session[:user_id] = @fb.id
      
-        redirect_to user_path(@user)
-      end
+        redirect_to fb_path(@fb)
+    end
      
+    def show
+        @fb = Fb.find(params[:id])
+    end
       private
      
       def auth
