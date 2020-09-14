effb8439ed24b3c9635593d060863e3df4af9325
Amonsta <amana.ammi@gmail.com>
2020-09-13 23:43:35 -0600

    validation for reaction

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -3,5 +3,6 @@ class Reaction < ApplicationRecord
   belongs_to :review
 
   validates :comment, presence: true
+  validates :review, uniqueness: {scope: :user, message: "-- can only be reacted to once."}
   
 end
