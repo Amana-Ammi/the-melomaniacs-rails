f8272b98492a617202b32c4f313dca234c7c03f3
Amonsta <amana.ammi@gmail.com>
2020-09-14 15:17:50 -0600

    fixed links for logged in users and vice versa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
     def home
     end
-
-    private
-
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
-    end
 end
--- app/controllers/fbs_controller.rb
+++ app/controllers/fbs_controller.rb
@@ -2,12 +2,11 @@ class FbsController < ApplicationController
     def create
         @fb = Fb.find_or_create_by(uid: auth['uid']) do |u|
           u.name = auth['info']['name']
-
           u.image = auth['info']['image']
+          @fb.save
         end
   #    binding.pry
         session[:user_id] = @fb.id
-     
         redirect_to fb_path(@fb)
     end
      
