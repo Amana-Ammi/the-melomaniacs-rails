a5deea746b5e3476655427ff38bc58237b41b15e
Amonsta <amana.ammi@gmail.com>
2020-09-21 10:56:11 -0600

    chained ar method, placed in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
+        # raise params.inspect
+        @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
@@ -18,15 +19,10 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
-    def spotify
-        spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-        # Now you can access user's private data, create playlists and much more
-      end
-
     private
 
     def user_params
-        params.require(:user).permit(:username,:email,:password)
+        params.require(:user).permit(:username, :email, :password)
     end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -6,4 +6,9 @@ class Review < ApplicationRecord
     validates :title, presence: true
     validates :title, uniqueness: true
     validates :content, presence: true
+
+    def self.order_by_time
+        Review.order(created_at: :desc).limit(4)
+    end
+
 end
